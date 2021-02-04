ae602a92fc7031f032f28917e61a70ccb5f14c0c
falisha-shoun <falisha.shoun@live.com>
2021-02-04 04:46:12 +0000

    add printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Use printf command.
+ * Return: 0 value.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

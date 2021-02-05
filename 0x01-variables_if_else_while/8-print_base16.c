194bf0ee7a27b5d197ab5d5c8af33dc2df63082b
falisha-shoun <falisha.shoun@live.com>
2021-02-05 05:08:56 +0000

    add 8-print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Print lower case hexadecimal numbers.
+ * Return: 0 value.
+*/
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+
+}

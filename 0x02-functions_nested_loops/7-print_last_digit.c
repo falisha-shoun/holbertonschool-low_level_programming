160f9e804242664a6135982bfeb09bd6a667002e
falisha-shoun <falisha.shoun@live.com>
2021-02-10 03:30:02 +0000

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * print_last_digit - prints last int digit.
- * @n: int tested.
- * Return: last digit.
+ * print_last_digit - Shows last digit.
+ * @n: find last digit.
+ * Return: prints last digit.
  */
 int print_last_digit(int n)
 {
 	if (n < 0)
 		n = n * -1;
-	_putchar((n % 10) + '0');
+	_putchar(n % 10 + '0');
 	return (n % 10);
 }

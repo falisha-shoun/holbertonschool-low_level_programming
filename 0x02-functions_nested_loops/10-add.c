3cf971a179c6d9e77c582eb367d264c2f145b1d1
falisha-shoun <falisha.shoun@live.com>
2021-02-10 04:17:06 +0000

    add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - adding int together.
+ * @i: 1st number.
+ * @j: 2nd number.
+ * Return: value of two ints.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}

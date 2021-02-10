9d8c679aa80f3b5ba5d136313ce3fd02b73d2340
falisha-shoun <falisha.shoun@live.com>
2021-02-10 02:12:13 +0000

    add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - prints if an int is +, -, or 0.
+ * @n: n is the int tested.
+ * Return: 1 for +, 0 for 0, & -1 for -.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

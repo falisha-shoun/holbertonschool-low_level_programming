e95c3974294a7fc351f61590bbfb38c2655b5821
falisha-shoun <falisha.shoun@live.com>
2021-02-12 01:30:37 +0000

    add more numbs

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 0-14 10x.
+ * Return: void.
+ */
+
+void more_numbers(void)
+{
+	int n, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}

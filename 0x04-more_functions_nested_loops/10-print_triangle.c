abeda86cc80de9b594e2504fcdab41b81717792f
falisha-shoun <falisha.shoun@live.com>
2021-02-12 02:08:45 +0000

    add triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle.
+ * @size: triangle size.
+ * Return: void.
+ */
+
+void print_triangle(int size)
+{
+	int i, c;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				if (c < size - i - 1)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
